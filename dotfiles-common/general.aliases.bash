a8122112cb85011129a373e3e4f8dac2d977f873
Lukasz Jader <ljaderdev@gmail.com>
2015-04-18 12:00:16 +0200

    Current version of my dotfiles
    From windows machine

---
+++ dotfiles-common/general.aliases.bash
@@ -0,0 +1,14 @@
+alias ll='ls -al'
+
+alias md='mkdir -p'
+alias rd='rmdir'
+
+alias 1up='..'
+alias 2up='../..'
+alias 3up='../../..'
+alias 4up='../../../..'
+
+#alias ..='cd ..'         # Go up one directory
+#alias ...='cd ../..'     # Go up two directories
+#alias ....='cd ../../..' # Go up two directories
+#alias -- -='cd -'        # Go back
---
+++ dotfiles-common/git.aliases.bash
@@ -0,0 +1,6 @@
+alias get='git'
+
+alias gitka='gitk --all'
+
+alias gitgui='git gui'
+alias ggui='git gui'
